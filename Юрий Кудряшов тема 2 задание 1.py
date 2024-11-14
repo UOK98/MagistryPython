6fb1699eba5a6da41ce4c6b532da9d3063e2917e
UOK98 <94374019+UOK98@users.noreply.github.com>
2024-11-14 18:05:38 +0300

    Add files via upload

--- Юрий Кудряшов тема 2 задание 1.py
+++ Юрий Кудряшов тема 2 задание 1.py
@@ -1,10 +1,10 @@
-word = "hippopotamus"
+word = "test"
 
 if len(word)%2 > 0:
-    symbol_number = int(len(word)/2+1)
+    symbol_number = int(int(len(word))/2)
     print(word[symbol_number])
 else:
-    symbol_number = int(len(word)/2)
+    symbol_number = int(int(len(word))/2)
     print(word[symbol_number-1] + word[symbol_number])
 
 
